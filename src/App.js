8558750480f961ee3d1266c725392bce9d4aa149
Bikash <102658104+bikashSpinshell@users.noreply.github.com>
2022-05-27 14:57:06 +0900

    adding cart context and shopping item

--- src/App.js
+++ src/App.js
@@ -3,16 +3,12 @@ import { Routes, Route } from 'react-router-dom';
 import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
 import Authentication from './routes/authentication/authentication.component';
-
-
-const Shop = () => {
-  return <h1> I am shop</h1>;
-}
+import Shop from './routes/shop/shop.component'
 
 const App = () => {
   return(
     <Routes>
-      <Route pat='/' element={ <Navigation />}>
+      <Route path='/' element={ <Navigation />}>
         <Route index element={ <Home /> }/>
         <Route path="shop" element={ <Shop /> } />
         <Route path="auth" element={ <Authentication /> } />
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -3,13 +3,17 @@ import { Outlet, Link } from 'react-router-dom';
 
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import { UserContext } from '../../contexts/user.context';
+import { CartContext } from '../../contexts/cart.context';
 
 import { signOutUser } from '../../utils/firebase/firebase.utils';
+import CartIcon from '../../components/cart-icon/cart-icon.components';
+import CartDropDown from '../../components/cart-dropdown/cart-dropdown.component';
 
 import './navigation.styles.scss';
 
 const Navigation = () => {
     const { currentUser } = useContext(UserContext);
+    const { isCartOpen } = useContext(CartContext);
 
     return (
         <Fragment>
@@ -31,8 +35,9 @@ const Navigation = () => {
                             )
 
                     }
-
+                    <CartIcon />
                 </div>
+                {isCartOpen && <CartDropDown />}
             </div>
             <Outlet />
         </Fragment>
